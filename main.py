6356e8707dbf0f74e2edc688543e293ccf6eba5b
connerpatton7 <connerpatton77@gmail.com>
2026-03-19 11:47:24 -0400

    push

---
+++ main.py
@@ -0,0 +1,12 @@
+from temperature import get_temperature
+
+# 39.3292° N, 82.1013° W
+def main():
+    latitude = 39.3292
+    longitude = -82.1013
+    temp_data = get_temperature(latitude, longitude, "")
+    
+    
+    
+if __name__ == "__main__":
+    main()
